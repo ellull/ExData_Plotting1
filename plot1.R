b7224aafb523de63f67e4c9cefdce5bd53760f5e
Eduard Llull <eduard@llull.net>
2014-12-03 00:03:30 +0100

    Add first plot script (plot1.R) and resulting PNG image (plot1.png)

---
+++ plot1.R
@@ -0,0 +1,11 @@
+# Get only the header and data from 2007-02-01 to 2007-02-02
+pipe <- pipe("egrep '^(Date|[12]/2/2007)' ./data/household_power_consumption.txt")
+dt <- read.table(pipe, header=TRUE, sep=";", na.strings="?")
+
+# Convert the Date and Time variables to a datetime
+dt <- transform(dt, DateTime=strptime(paste(as.character(Date), as.character(Time)), "%d/%m/%Y %H:%M:%S"))
+
+# Create the plot
+png("plot1.png", width=480, height=480)
+hist(dt$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
